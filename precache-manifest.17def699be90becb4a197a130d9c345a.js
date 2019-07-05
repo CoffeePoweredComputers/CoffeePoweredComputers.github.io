8d7584fae6023dc782815749d8adbfecb711b975
David H Smith IV <smithd77@wwu.edu>
2019-07-05 00:32:42 -0700

    Updates

---
+++ precache-manifest.17def699be90becb4a197a130d9c345a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "238c8a6a19ac43bf5ab8",
+    "url": "/static/css/main.8df251e3.chunk.css"
+  },
+  {
+    "revision": "238c8a6a19ac43bf5ab8",
+    "url": "/static/js/main.238c8a6a.chunk.js"
+  },
+  {
+    "revision": "a31728b1a85a66adaa23",
+    "url": "/static/js/1.a31728b1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0eb687ee70e260f51951a9a56df521ed",
+    "url": "/index.html"
+  }
+];
