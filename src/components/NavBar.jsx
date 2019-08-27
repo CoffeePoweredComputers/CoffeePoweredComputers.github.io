55c025fe3320917b29908c07109aa8ca0cd6c7fd
David H Smith IV <smithd77@wwu.edu>
2019-08-26 22:36:26 -0700

    fixed the blog button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,9 +15,9 @@ export default class NavBar extends Component {
 				<Navbar.Collapse>
 					<Nav>
 						<NavItem eventKey={1} componentClass={Link} href="/" to='/'> Home </NavItem>
-						<NavItem eventKey={3} componentClass={Link} href="/publications" to='/publications'> Publications </NavItem>
-						<NavItem eventKey={2} componentClass={Link} href="/blog" to='/blog'> Blog </NavItem>
-						<NavItem eventKey={3} componentClass={Link} href="/cv" to='/cv'> Curriculum Vitae </NavItem>
+						<NavItem eventKey={2} componentClass={Link} href="/publications" to='/publications'> Publications </NavItem>
+						<NavItem eventKey={3} className="nav-link" href="https://hamiltonfour.tech/blog" target="_blank" > Blog </NavItem>
+						<NavItem eventKey={4} componentClass={Link} href="/cv" to='/cv'> Curriculum Vitae </NavItem>
 					</Nav>
 				</Navbar.Collapse>
 			</Navbar >
