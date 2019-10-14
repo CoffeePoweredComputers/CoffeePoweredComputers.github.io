b3e68c9cada2561db0f09d72dfe5594e6b640337
smithd77 <smithd77@wwu.edu>
2019-10-14 14:23:10 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "coffeepoweredcomputers.github.io/precache-manifest.0cbf1951b2835c40bcc7c12a13abd052.js"
+  "/precache-manifest.d0bd02ea4c36ff958b93960f055fe23b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("coffeepoweredcomputers.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
