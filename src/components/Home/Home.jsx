1575a2cccd8e9cb4d4428e77a975c0ac06b8e374
David H Smith IV <davidsmith@hamiltonfour.tech>
2021-12-30 14:42:04 -0600

    added new files

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -51,10 +51,8 @@ export default class Home extends Component {
             <Col className='bio bio-alignment'>
 
               <b>
-                I am a first year PhD student at the University of Illinois Urbana-Champaign in the Department of Computer Science. </b>
-              My research focus lies at the intersection of data science and Human-Computer Interaction (HCI) specifically within the realm of Computing Education Research (CER).
-              The core of my knowledge and interests in these subjects have been developed through my role as an undergraduate research assistant in which my time has been largely devoted to the analysis of student learning patterns, performance, and tool evaluation. 
-              Going forward, it is my goal to extend and refine my current skills as well as continue investigating the intersection between HCI and CER through the development and evaluation of tools aimed towards usage in educational environments. 
+                  I am a second PhD student at the University of Illinois Urbana-Champaign in the Department of Computer Science. </b>
+              My current research focus in the development and administration of randomized exams as well as introductory computer science education. 
 
             </Col>
           </Row>
